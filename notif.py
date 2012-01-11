db8ef2a3792a4b650e4774f937dda1847003ec4a
spoljo <spoljo@diznilend.org>
2012-01-11 19:47:34 +0100

    Fixed bug with double checking mail

--- notif.py
+++ notif.py
@@ -4,6 +4,7 @@ import time,imaplib,os,argparse
 from sys import exit
 import pynotify as pyn
 
+
 def notif_gen(n):
 	return pyn.Notification("You have mail.","New messages:"+str(n))
 
@@ -43,8 +44,9 @@ if __name__ == "__main__" :
 	tmp = checkmail(c[0],c[1],c[2])
 	notif_gen(tmp).show()
 	while True:
-		if checkmail(c[0],c[1],c[2]) > tmp:
-			tmp=checkmail(c[0],c[1],c[2])
+		a = checkmail(c[0],c[1],c[2])
+		if a > tmp:
+			tmp=a
 			notif_gen(tmp).show()
 		else:
 			time.sleep(c[3])
